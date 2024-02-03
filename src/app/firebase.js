952118ac20e016c74de8ace67de18c5f9da65656
Zephneko <zehan.abrar@gmail.com>
2024-02-03 17:43:19 -0500

    added changes to  backend stuff

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,8 +1,15 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  // ... your firebase config
+    apiKey: "AIzaSyAU1BR8bbeWbB0JDnyw9LPikbgrrjTnjcg",
+    authDomain: "hack-violet-413019.firebaseapp.com",
+    projectId: "hack-violet-413019",
+    storageBucket: "hack-violet-413019.appspot.com",
+    messagingSenderId: "161905241602",
+    appId: "1:161905241602:web:9ae17132068ed9b5d6c11b",
+    measurementId: "G-JM84VG10MF"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
